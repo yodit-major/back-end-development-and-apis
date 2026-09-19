da2b0764001404cadd080d3c7c7a3439e7ad40f8
yoditmajor <myodit6@gmail.com>
2026-09-19 23:42:19 +0000

    Build prime number checker module

---
+++ build-a-prime-number-checker-module/index.js
@@ -0,0 +1,16 @@
+function isPrime(num) {
+    if (num > 1) {
+        for (let i = 2; i <= num - 1; i++) {
+            if (num % i === 0) {
+                return false;
+            }
+        }
+        return true;
+    } else {
+        return false;
+    }
+}
+
+module.exports = {
+    isPrime
+};
